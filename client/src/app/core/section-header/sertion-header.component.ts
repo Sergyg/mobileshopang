b8672c0e6e0e93acd9ba34194a9face87e6bb798
Sergyg <golovaty.s.v@gmail.com>
2023-01-10 19:51:55 +0100

    6

---
+++ client/src/app/core/section-header/sertion-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sertion-header',
+  templateUrl: './sertion-header.component.html',
+  styleUrls: ['./sertion-header.component.scss']
+})
+export class SertionHeaderComponent {
+
+}
